43a9fab2d26065f884793280aa7074b1c8ce5c28
leitzkemariana <leitzkemariana@gmail.com>
2025-10-16 09:26:29 -0300

    update table users to username and email be unique

--- src/main/resources/db/migration/V20250913115900__initial_state.sql
+++ src/main/resources/db/migration/V20250913115900__initial_state.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id       INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    email    VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
